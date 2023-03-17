fd3605ea4bedfd17c0ad5b19ab4348ef1ed21e4a
azizy-123 <abdelazizeabdullahelsouday@gmail.com>
2023-03-17 07:17:41 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point 
  *
@@ -6,8 +7,9 @@
  *
  * Return: Alawys 0 (Success)
 */
+
 int main(void) 
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 } 	
