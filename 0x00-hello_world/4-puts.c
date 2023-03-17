a5b231545a1a3a8c694e23c07163fc3efb62b390
azizy-123 <abdelazizeabdullahelsouday@gmail.com>
2023-03-17 07:12:07 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stido.h>
+/**
+ * main - Entry point 
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+int main(void) 
+{
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+} 	
