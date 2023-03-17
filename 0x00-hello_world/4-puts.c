7847912fd5b8df922e2f1a7ace2c1df731b783e0
azizy-123 <abdelazizeabdullahelsouday@gmail.com>
2023-03-17 07:41:29 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
-#include<unistd.h> 
- 
-/** 
- * main - Entry point 
- * 
- * Description: print a qoute using write function 
- *    ssize_t write(int fd, const void *buf, size_t count); 
- * 
- * Return: 1 (error). 
-*/ 
- 
- 
-int main(void) 
-{ char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
- 
- write(1, quo, 59); 
- return (1); 
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 } 
