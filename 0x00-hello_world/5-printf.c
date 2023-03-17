766f5f76260c0b3a6ec83c513de77ae2f38fafd0
azizy-123 <abdelazizeabdullahelsouday@gmail.com>
2023-03-17 08:00:44 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Descrition: A C program that prints with printf function
+ * Description: A C progam that prints with printf function.
  *
- * Return: Alawys 0 (success)
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
