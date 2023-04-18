fe85aa8bafa675276af1677906bfa1d8a117596a
azizy-123 <abdelazizeabdullahelsouday@gmail.com>
2023-04-18 10:34:04 -0700

    good

---
+++ 0x0E-structures_typedef/_putchar.c
@@ -0,0 +1,27 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#endif /* MAIN_H */
+
+
+int _putchar(char c);
+
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1.
+ *      On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
