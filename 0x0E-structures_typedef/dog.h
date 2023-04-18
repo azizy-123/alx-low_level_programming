5b7b4b35a5ae06736db6733af27e0cefec17cfc5
azizy-123 <abdelazizeabdullahelsouday@gmail.com>
2023-04-18 10:16:03 -0700

    d

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,25 @@
-#ifndef ODG_H
-#define ODG_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog -a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
- *
- * Description: just a lone dog struct in a big kitty world
+ * struct dog - Entry point
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: Define struct dog with the three members
  */
+
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
